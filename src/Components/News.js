09cf93c4cd8c3ef95f8498ad476e40ea54761536
pseudocode21 <151240191+pseudocode21@users.noreply.github.com>
2025-09-12 23:45:34 +0530

    Added Infinite Scrolling Successfully

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 export default class News extends Component {
   static defaultProps = {
     country: "us",
@@ -20,8 +22,9 @@ export default class News extends Component {
     super(props);
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
+      totalResults: 0,
     };
     document.title = `${this.capitalize(this.props.category)} - News Monkey`;
   }
@@ -58,52 +61,51 @@ export default class News extends Component {
     );
   };
 
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2b5483ae48e145498425f0605e7cb890&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+    });
+  };
   render() {
     return (
-      <div className="container my-4  ">
+      <>
         <h1 className="text-center" style={{ marginBottom: "2rem" }}>
           NewsMonkey - Top {this.capitalize(this.props.category)} Headlines
         </h1>
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {this.state.articles.map((elem) => {
-            return (
-              <div className="col-md-4" key={elem.title}>
-                <NewsItem
-                  title={elem.title ? elem.title : ""}
-                  description={elem.description ? elem.description : ""}
-                  imageUrl={elem.urlToImage}
-                  newsUrl={elem.url}
-                  author={elem.author}
-                  date={elem.publishedAt}
-                  source={elem.source.name}
-                />
-              </div>
-            );
-          })}
-        </div>
-        <div className="container d-flex justify-content-between my-3">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            onClick={this.handlePrevClick}
-            className="btn btn-dark"
-          >
-            &laquo; previous
-          </button>
-          <button
-            disabled={
-              this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.props.pageSize)
-            }
-            type="button "
-            onClick={this.handleNextClick}
-            className="btn btn-dark "
-          >
-            Next &raquo;
-          </button>
-        </div>
-      </div>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length < this.state.totalResults}
+          loader={<Spinner />}
+          endMessage={<div> that's it</div>}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((elem) => {
+                return (
+                  <div className="col-md-4" key={elem.title}>
+                    <NewsItem
+                      title={elem.title ? elem.title : ""}
+                      description={elem.description ? elem.description : ""}
+                      imageUrl={elem.urlToImage}
+                      newsUrl={elem.url}
+                      author={elem.author}
+                      date={elem.publishedAt}
+                      source={elem.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
     );
   }
 }
